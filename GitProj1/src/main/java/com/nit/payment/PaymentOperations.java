822ca3458c60311115ecf9bf5d5aeee7fa9445f2
Konda Venkat Sai <Konda Venkat Sai@192.168.1.65>
2024-02-28 21:59:38 +0530

    dev1 written payment code

--- GitProj1/src/main/java/com/nit/payment/PaymentOperations.java
+++ GitProj1/src/main/java/com/nit/payment/PaymentOperations.java
@@ -1,5 +1,15 @@
 package com.nit.payment;
 
 public class PaymentOperations {
+	
+	public String debitCardPayment() {
+		
+		return "debit card payment done";
+	}
+	
+	public String creditCardPayment() {
+		
+		return "credit card payment done";
+	}
 
 }
